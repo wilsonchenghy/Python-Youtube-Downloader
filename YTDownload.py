731b3ed64ff9ccd1f089cf1c4155ee3b07e1030d
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-01-20 15:14:49 -0500

    Update YTDownload.py

--- YTDownload.py
+++ YTDownload.py
@@ -3,18 +3,51 @@
 # pytube got a problem with mac in which using yt.streams.filter(only_audio=True).first() give an audio file that is twice as long and the file format remains .mp4
 # so the solution is to extract the video file first (mp4 file) using pytube, and afterwards converting it to mp3 through ffmpeg
 
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import subprocess
 
-YouTubeUrl = input("Enter YouTubeUrl: ")
-yt = YouTube(YouTubeUrl)
-video = yt.streams.get_highest_resolution()
 
-videoPath = video.download()
-print(videoPath)
-audioPath = videoPath.replace(".mp4", ".mp3")
-print(audioPath)
 
-subprocess.run(['ffmpeg', '-i', videoPath, '-q:a', '0', '-map', 'a', audioPath])
+def downloadVideo(url):
+    yt = YouTube(url)
+    video = yt.streams.get_highest_resolution()
 
-print("Done")
+    videoPath = video.download()
+    print(videoPath)
+    audioPath = videoPath.replace(".mp4", ".mp3")
+    print(audioPath)
+
+    subprocess.run(['ffmpeg', '-i', videoPath, '-q:a', '0', '-map', 'a', audioPath])
+
+
+
+print(" ")
+print("Option 1: download with individual URL")
+print("Option 2: download with playlist URL")
+print(" ")
+
+option = input("Choose Option: ")
+
+if option == "1":
+
+    print(" ")
+    YouTubeUrl = input("Enter YouTubeURL: ")
+    downloadVideo(YouTubeUrl)
+
+    print("Done")
+
+elif option == "2":
+
+    print(" ")
+    playlistUrl = input("Enter Playlist URL: ")
+    print(" ")
+    playlist = Playlist(playlistUrl)
+
+    # show the number of videos contained in the playlist
+    print(f"Number of videos in the playlist: {len(playlist.video_urls)}")
+
+    for url in playlist:
+        downloadVideo(url)
+
+else:
+    print("Inputted Option Incorrect")
